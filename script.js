c7d79ed8476ed3d6f74d55882a7ad478249e1803
rjammazi87 <rjammazi@gmail.com>
2020-08-31 17:57:00 +0100

    MAJ du fichier script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function affichPopUp{
-	var stay=confirm("Bonjour, voulez-vous aller vers le site xxxvidsxxx ?");
-if (stay){
-	window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}
-}
-
+var stay=confirm("Bonjour, voulez-vous aller vers le site xxxvidsxxx ?");
+if (stay)
+	window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
