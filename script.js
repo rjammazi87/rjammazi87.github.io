f2d30e114810c01004ef77f4f0b8a24c223a0da1
rjammazi87 <rjammazi@gmail.com>
2020-08-31 17:16:48 +0100

    Ajout du fichier script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function affichPopUp{
+	var stay=confirm("Bonjour, voulez-vous aller vers le site xxxvidsxxx ?");
+if (stay){
+	window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+}
+
